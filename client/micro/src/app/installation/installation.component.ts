29df1229703b4eaa9b16ab1fcefa5c4aab5bb5f3
Kyarunts Davit <kyaruntsdavid@gmail.com>
2018-02-25 22:24:21 +0400

    Carusel in INstallation

--- client/micro/src/app/installation/installation.component.ts
+++ client/micro/src/app/installation/installation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-installation',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./installation.component.scss']
 })
 export class InstallationComponent implements OnInit {
+
+    @ViewChild('carusel') carusel: ElementRef;
 
     public images: Object[] = [
         { id: '0', image: 'http://bonusbuildings.com/imgs/gall_content/61a46ad.jpeg'},
@@ -34,10 +36,36 @@ export class InstallationComponent implements OnInit {
         { id: '3', image: 'http://bonusbuildings.com/imgs/gall_content/61a46ad.jpeg' },
         { id: '4', image: 'http://bonusbuildings.com/imgs/gall_content/61a46ad.jpeg' },
     ]
+    private timer: any;
     constructor() { }
     
     ngOnInit() {
         window.document.body.scrollIntoView(true);
     }
+
+    public scrollTo(direction: 'right' | 'left'): void {
+        if (direction === 'right') {
+            this.carusel.nativeElement.scrollTo(
+                this.carusel.nativeElement.scrollLeft + 180, 0
+            );
+        } else {
+            this.carusel.nativeElement.scrollTo(
+                this.carusel.nativeElement.scrollLeft - 180, 0
+            );
+        }
+        
+    }
     
+    public mousedown(direction: 'right' | 'left'): void {
+        this.timer = setInterval(() => {
+            this.scrollTo(direction);
+        }, 50);
+    }
+
+    public mouseup(): void {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
 }
