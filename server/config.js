62c38a7cdcbddc4c53444770004712ed2ac35472
Kyarunts Davit <kyaruntsdavid@gmail.com>
2018-03-21 22:49:44 +0400

    Setup build commands

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'serverport':2017,
-    'tokenexp': 60 * 60 * 24 * 1000,
-    'secret': 'mysecretkey',
-    'database': 'mongodb://micro:micro2018@ds127506.mlab.com:27506/micro'
- };
--- server/src/app/routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var appRouter = express.Router();
-
-var partnerHandlers = require('./controllers/partner');
-
-appRouter.get('/partners', partnerHandlers.getByQuery);
-
-
-module.exports = appRouter;
