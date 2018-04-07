be96baedf4402a0fb354d88fd340873478a670d7
David Kyarunts <kyarunts@MacBook-Pro-David.local>
2018-04-07 11:44:35 +0400

    Contacts page

--- client/micro/src/app/contacts/contacts.component.ts
+++ client/micro/src/app/contacts/contacts.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+    selector: 'app-contacts',
+    templateUrl: './contacts.component.html',
+    styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+    private re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    public showForm: boolean = true;
+    public formObject: Object = {
+        name: '',
+        email: '',
+        message: '',
+    }
+    public formErrors: Object = {
+        name: false,
+        email: false,
+        message: false,
+    }
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public validate() {
+        if (!this.formObject['name'].trim()) {
+            this.formErrors['name'] = true;
+        }
+        if (
+            !this.formObject['email'].trim() ||
+            !this.re.test(this.formObject['email'].toLowerCase())
+        ) {
+            this.formErrors['email'] = true;
+        }
+        if (!this.formObject['message'].trim()) {
+            this.formErrors['message'] = true;
+        }
+        if (!this.formErrors['name'] && !this.formErrors['email'] && !this.formErrors['message']) {
+            console.log('should be sent');
+            this.showForm = false;
+            this.formObject['name'] = '';
+            this.formObject['email'] = '';
+            this.formObject['message'] = '';
+            
+        }
+    }
+
+    public validateOnInput(formControl) {
+        if (this.formObject[formControl].replace(' ', '')) {
+            this.formErrors[formControl] = false;
+        }
+    }
 }
