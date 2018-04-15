90063c6b2aaf9d727a193ef0f7a143dacec03fa7
Stepan Vardanyan <styopdev@hotmail.com>
2018-04-15 14:49:55 +0400

    Backend initial setup

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-var path = require('path')
-
-app.use(express.static(__dirname + '/dist'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
