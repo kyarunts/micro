3dc2992730131b7602b7a0eda9945a2805f50486
Stepan Vardanyan <styopdev@hotmail.com>
2018-04-17 17:19:58 +0400

    Added translation module

--- client/micro/src/app/app.module.ts
+++ client/micro/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -54,8 +57,15 @@ import { ImageViewerComponent } from './generics/image-viewer/image-viewer.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule,
-    FormsModule    
+    HttpClientModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     MicroHttpService,
@@ -65,3 +75,7 @@ import { ImageViewerComponent } from './generics/image-viewer/image-viewer.compo
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
